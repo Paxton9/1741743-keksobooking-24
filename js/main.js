3b865b99504fe1d116b00c38140cbcbb314ec566
Paxton9 <81966445+Paxton9@users.noreply.github.com>
2021-09-30 12:27:37 +0300

    functions

--- js/main.js
+++ js/main.js
@@ -1,10 +1,39 @@
-const getRandom = function(min , max) {
-  min = 0;
-  max = 10;
-  if (max < 0) {
-    return Math.floor(Math.random() * -max);
+
+//  Первая функция , радномное целое число.
+
+const getRandom = function (min, max) {
+  min = 11;
+  max = 99;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max < min) {
+    max = Math.ceil(max);
+    min = Math.floor(min);
+
+    return Math.floor(Math.random() * (min - max + 1)) + max;
+
   }
-  return Math.floor(Math.random() * max);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+
 };
 
 getRandom();
+
+
+//  Вторая функция , рандомное число с плавающей точкой.
+
+const fractionNumbers = function(min , max ) {
+  min = 0;
+  max = 25;
+
+  if (max < min) {
+    return Number((Math.random() * (max - min + 1) + min).toFixed(5));
+  }
+
+  return Number((Math.random() * (min - max + 1) + max).toFixed(5));
+};
+
+fractionNumbers();
+
+
