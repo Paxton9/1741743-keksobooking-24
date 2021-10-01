9f26988911209ee132f4133addfe30803f83620a
Paxton9 <81966445+Paxton9@users.noreply.github.com>
2021-10-01 11:50:25 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -2,16 +2,17 @@
 //  Первая функция , радномное целое число.
 
 const getRandom = function (min, max) {
-  min = 11;
-  max = 99;
+
   min = Math.ceil(min);
   max = Math.floor(max);
   if (max < min) {
-    max = Math.ceil(max);
-    min = Math.floor(min);
 
-    return Math.floor(Math.random() * (min - max + 1)) + max;
+    [max, min] = [min, max];
 
+  }
+
+  if (min < 0 || max < 0) {
+    throw new Error('Нельзя использовать отрицательные числа')
   }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -23,17 +24,23 @@ getRandom();
 
 //  Вторая функция , рандомное число с плавающей точкой.
 
-const fractionNumbers = function(min , max ) {
-  min = 0;
-  max = 25;
+const getFractionNumbers = function(min , max , symbols ) {
 
   if (max < min) {
-    return Number((Math.random() * (max - min + 1) + min).toFixed(5));
-  }
 
-  return Number((Math.random() * (min - max + 1) + max).toFixed(5));
+    [max, min] = [min, max];
+
+  }
+  return Number((Math.random() * (max - min) + min).toFixed(symbols));
+
 };
 
-fractionNumbers();
+getFractionNumbers();
 
 
+
+
+
+if ([min , max] < 0) {
+  return Math.floor(Math.random() * ((max - min + 1)) + min) * -1;
+}
