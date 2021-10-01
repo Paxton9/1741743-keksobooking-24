d8e53b4cde2abaff1e12d688c53d6d6156c5e0ed
Paxton9 <81966445+Paxton9@users.noreply.github.com>
2021-10-01 22:33:53 +0300

    refactoor2

--- js/main.js
+++ js/main.js
@@ -1,46 +1,24 @@
-
 //  Первая функция , радномное целое число.
 
 const getRandom = function (min, max) {
-
   min = Math.ceil(min);
   max = Math.floor(max);
   if (max < min) {
-
     [max, min] = [min, max];
-
   }
-
   if (min < 0 || max < 0) {
     throw new Error('Нельзя использовать отрицательные числа')
   }
-
   return Math.floor(Math.random() * (max - min + 1)) + min;
-
 };
-
-getRandom();
-
+getRandom(2 , 15);
 
 //  Вторая функция , рандомное число с плавающей точкой.
 
 const getFractionNumbers = function(min , max , symbols ) {
-
   if (max < min) {
-
     [max, min] = [min, max];
-
   }
   return Number((Math.random() * (max - min) + min).toFixed(symbols));
-
 };
-
-getFractionNumbers();
-
-
-
-
-
-if ([min , max] < 0) {
-  return Math.floor(Math.random() * ((max - min + 1)) + min) * -1;
-}
+getFractionNumbers(3.2 , 7.7 , 3);
