fc183a275ca82e89182c3568a8fc357baf05f187
Paxton9 <81966445+Paxton9@users.noreply.github.com>
2021-10-09 08:54:17 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //  Первая функция , радномное целое число.
 
-const AVATAR_RANDOM_INT = _.shuffle(['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']);
+const randomInts = _.shuffle(['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']);
 
 
 const AD_TITLES = ['Проведите лучшую ночь в своей жизни , в компании друзей', 'Получите незабываемые впечатления', 'Удовольствие гарантировано', 'Каждый должен здесь побывать', 'Здесь вам рады', 'Почувствуйте себя царями', 'Здесь бывали Президенты'];
@@ -32,6 +32,11 @@ const COMPILATION_COUNT = 10;
 
 const SYMBOLS_COUNT = 5;
 
+const BEGIN_LAT = 35.65000;
+const END_LAT = 35.70000;
+const BEGIN_LONG = 139.70000;
+const END_LONG = 139.80000;
+
 const ROOM_INFOS = ['Помещение чистое , ухоженное , солнечная сторона.', 'Хороший , уютный дом', 'Прекрасная квартирка', 'Оборудованное помешение для комфортного жилья', 'Замечательное место , торговые центры в шаговой доступности', 'Новый Hitech ремонт и просторные комнаты',]
 
 const PHOTOS = _.shuffle([
@@ -41,35 +46,32 @@ const PHOTOS = _.shuffle([
 ]);
 
 
-const getRandomArrayElement = (elements) => {
-  return elements[_.random(0, elements.length - 1)];
-};
 
 const createAd = () => {
-  const randomLong = _.random(35.65000, 35.70000).toFixed(SYMBOLS_COUNT);
-  const randomLat = _.random(139.70000, 139.80000).toFixed(SYMBOLS_COUNT);
+  const randomLat = _.random(BEGIN_LAT, END_LAT).toFixed(SYMBOLS_COUNT);
+  const randomLong = _.random(BEGIN_LONG, END_LONG).toFixed(SYMBOLS_COUNT);
   return {
     author: {
-      avatar: 'img/avatars/user' + AVATAR_RANDOM_INT.shift() + '.png'},
+      avatar: 'img/avatars/user' + randomInts.shift() + '.png'},
     offer: {
-      title: getRandomArrayElement(AD_TITLES),
+      title: _.sample(AD_TITLES),
       address:  randomLong + ' ' + randomLat,
-      price: [_.random(60, 830)],
-      type: getRandomArrayElement(HOUSE_TYPES),
-      rooms: [_.random(2, 8)],
-      guests: [_.random(2, 12)],
-      checkin: getRandomArrayElement(CHECK_INS),
-      checkout: getRandomArrayElement(CHECK_OUTS),
+      price: _.random(60, 830),
+      type: _.sample(HOUSE_TYPES),
+      rooms: _.random(2, 8),
+      guests: _.random(2, 12),
+      checkin: _.sample(CHECK_INS),
+      checkout: _.sample(CHECK_OUTS),
       features: SERVICES.slice( _.random(0 , 3) ,  _.random(4 , 6)),
-      description: getRandomArrayElement(ROOM_INFOS),
+      description: _.sample(ROOM_INFOS),
       photos: PHOTOS.slice(0, _.random(1 , 3)),
     },
     location: {
-      lat: randomLong,
-      lng: randomLat,
+      lat: randomLat,
+      lng: randomLong ,
     },
   };
 };
 
-const ANNOUNCEMENTS = Array.from({length: COMPILATION_COUNT}, createAd);
-console.log(ANNOUNCEMENTS);
+const announcements = Array.from({length: COMPILATION_COUNT}, createAd);
+console.log(announcements);
